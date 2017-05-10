44f970dcf7bf777f5749ffce9b35b1f0bd6709c8
xiaocongsheng <xiaocongsheng@cishoo.com>
2017-05-10 10:25:50 +0800

    jingtaiyemian

---
+++ new_admin/production/js/cishoo.js
@@ -0,0 +1,8 @@
+/**
+ * Created by wangshaohua on 2017/4/24.
+ */
+$(document).ready(function () {
+    $(".wsh_left").load("../common/left.html");
+    $(".wsh_top").load("../common/top.html");
+    init_sidebar()
+});
