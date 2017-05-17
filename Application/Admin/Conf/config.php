800b49dbe220ff7461176024ce4cae3eae668037
xiaocongsheng <xiaocongsheng@cishoo.com>
2017-05-17 10:54:59 +0800

    配置接口路径

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -106,4 +106,8 @@ return array(
     'TMPL_ACTION_SUCCESS' => MODULE_PATH . 'View/Public/success.html', // 默认成功跳转对应的模板文件
     'TMPL_EXCEPTION_FILE' => MODULE_PATH . 'View/Public/exception.html',// 异常页面的模板文件
 
+    'INTERFACR_API' => array(
+        'agent_create' => 'http://service/org/agent/create',
+        'get_user' => 'http://service/sys/user/get',
+    ),
 );
