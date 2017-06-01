fb9b43232b95b61c0814ee5b1674c133ed89ff4a
xiaocongsheng <xiaocongsheng@cishoo.com>
2017-06-01 15:24:54 +0800

    新建老人基础信息

--- Application/Admin/Controller/HealthController.class.php
+++ Application/Admin/Controller/HealthController.class.php
@@ -6,7 +6,30 @@ class HealthController extends AdminController
     function index()
     {
         $this->meta_title = "健康监控";
+        $pageNo = I('get.p', 1);
+        $name = I('get.name');
+        $param = think_json_encode(['name' => $name]);
+        if (empty($name)) {
+            $param = "{}";
+        }
+        $url = $this->getUrl('health_query') . '?pageNo=' . $pageNo . '&pageSize=' . C('PAGE_SIZE');
+        $list = $this->lists($url, $param);
+        foreach ($list['itemList'] as &$val) {
+            foreach ($val['deviceIdentifiers'] as $value) {
+                $val['device_str'] .= $value . ';';
+            }
+        }
+        $this->assign('list', $list['itemList']);
         $this->display();
+    }
+
+    //历史数据
+    function history()
+    {
+        $peopleId = I('get.peopleId');
+        $url = $this->getUrl('history') . $peopleId;
+        $response = http($url, null, 'GET');
+        print_r($response);
     }
 
     function add()
