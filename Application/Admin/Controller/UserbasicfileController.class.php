8d1955e5e678b06c0ec9b3de893ef17fcaa96693
diane <1428319760@qq.com>
2017-07-12 15:21:45 +0800

    分页15条 新建老人基础档案页面跳转；编号自动生成随机数； 设备报警样式

--- Application/Admin/Controller/UserbasicfileController.class.php
+++ Application/Admin/Controller/UserbasicfileController.class.php
@@ -117,15 +117,42 @@ class UserbasicfileController extends AdminController
         $url = $this->getUrl('people_save_edit');
         $response = http_post_json($url, $request);
         if ($response) {
-            $this->success("保存成功");
+            if ($peopleId) {
+                $this->success("保存成功");
+            }
+            else{
+                $this->success("保存成功",U('basicfile'));
+            }
         } else {
             $this->error("保存失败");
         }
+    }
+    //随机数
+    function GetRandStr($len)
+    {
+        $chars = array(
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+            "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+            "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G",
+            "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+            "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2",
+            "3", "4", "5", "6", "7", "8", "9"
+        );
+        $charsLen = count($chars) - 1;
+        shuffle($chars);
+        $output = "";
+        for ($i=0; $i<$len; $i++)
+        {
+            $output .= $chars[mt_rand(0, $charsLen)];
+        }
+        return $output;
     }
 
     function newadd()
     {
         $this->meta_title = "老人健康档案信息";
+        $identifier = $this->GetRandStr(4);
+        $this->assign('identifier',$identifier);
         $ethnicity = C('ETHNICITY');
         $education = C('EDUCATION');
         $economy = C('ECONOMY');
