62d31057da7877f03b0cdcf1776960189530680f
diane <1428319760@qq.com>
2017-07-14 16:21:57 +0800

    坐席首页隐藏块

--- Application/Admin/Controller/AdminController.class.php
+++ Application/Admin/Controller/AdminController.class.php
@@ -65,22 +65,6 @@ class AdminController extends Controller
         }
     }
 
-//    private function __get($property_name)
-//    {
-//        if(isset($this->$property_name))
-//        {
-//            return($this->$property_name);
-//        }
-//        else
-//        {
-//            return(NULL);
-//        }
-//    }
-//    private function __set($property_name, $value)
-//    {
-//        $this->$property_name = $value;
-//    }
-
 //获取当前用户权限
     protected function getRight()
     {
