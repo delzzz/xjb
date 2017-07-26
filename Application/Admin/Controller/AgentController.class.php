960adaad47f1217fe0b68df42e119ebb74aff875
diane <1428319760@qq.com>
2017-07-26 15:35:51 +0800

    托管总计

--- Application/Admin/Controller/AgentController.class.php
+++ Application/Admin/Controller/AgentController.class.php
@@ -50,19 +50,20 @@ class AgentController extends AdminController
             }
             //查询是否有代理商托管给自己
             $targetAgents = get_queryTarget($agentId);
-
             foreach ($targetAgents as $key => $value) {
                 if ($value['targetId'] != $agentId) {
                     unset($targetAgents[$key]);
                 }
             }
-
             if(count($targetAgents)>0){
                   $this->assign('change',1);
             }
             foreach ($targetAgents as $k => $v) {
                 $collocationInfo[] = $this->collocationList($v['sourceId'], $v['targetId'], $v['collocationId'], 2);
             }
+        }
+        foreach ($collocationInfo as $key=>&$value){
+            $value['statistics'] = agent_statistics($value['sourceId']);
         }
         $this->assign('collocationInfo', $collocationInfo);
         $this->assign('agentList', $this->agentList());
