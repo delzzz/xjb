3969239b9d40de52667b83b4a5310ce2cb8b036c
diane <1428319760@qq.com>
2018-05-23 11:04:30 +0800

    删除

--- new_admin/production/js/cishoo.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by wangshaohua on 2017/4/24.
- */
-$(document).ready(function () {
-    $(".wsh_left").load("../common/left.html");
-    $(".wsh_top").load("../common/top.html");
-    init_sidebar()
-});
